26c45901c040f761a4cca5e5cd91a534dcd64053
WinterWood0118 <dltjdals125@gmail.com>
2024-03-26 12:01:29 +0900

    3.3
    onClick 함수로 바꿈

--- app/src/main/java/com/example/week4_homework/SignInActivity.kt
+++ app/src/main/java/com/example/week4_homework/SignInActivity.kt
@@ -9,43 +9,50 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
-
-    lateinit var startForResult: ActivityResultLauncher<Intent>
+    private lateinit var loginBtn: Button
+    private lateinit var joinBtn: Button
+    private lateinit var idET: EditText
+    private lateinit var passwordET: EditText
+    private lateinit var startForResult: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val loginBtn = findViewById<Button>(R.id.btn_login)
-        val joinBtn = findViewById<Button>(R.id.btn_join)
-        val idET = findViewById<EditText>(R.id.et_id)
-        val passwordET = findViewById<EditText>(R.id.et_password)
+        loginBtn = findViewById(R.id.btn_login)
+        joinBtn = findViewById(R.id.btn_join)
+        idET = findViewById(R.id.et_id)
+        passwordET = findViewById(R.id.et_password)
 
-        startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-            if(result.resultCode == RESULT_OK){
-                val id = result.data?.getStringExtra("signed_id")
-                val password = result.data?.getStringExtra("signed_password")
-                idET.setText(id)
-                passwordET.setText(password)
-            }
-        }
-
-        loginBtn.setOnClickListener {
-            if(idET.text.isEmpty() || passwordET.text.isEmpty()){
-                toastFun(this,"아이디/비밀번호를 확인해주세요.")
-            }else {
-                val intent = Intent(this, HomeActivity::class.java)
-                intent.apply{
-                    putExtra("extra_id", idET.text.toString())
-                    putExtra("random", getRandomIndex())
+        startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val id = result.data?.getStringExtra("signed_id")
+                    val password = result.data?.getStringExtra("signed_password")
+                    idET.setText(id)
+                    passwordET.setText(password)
                 }
-                toastFun(this, "로그인 성공")
-                startActivity(intent)
             }
-        }
-        joinBtn.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startForResult.launch(intent)
-        }
 
+        loginBtn.setOnClickListener { loginBtnClickListener() }
+        joinBtn.setOnClickListener { joinBtnClickListener() }
+    }
+
+    private fun joinBtnClickListener() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startForResult.launch(intent)
+    }
+
+    private fun loginBtnClickListener() {
+        if (idET.text.isEmpty() || passwordET.text.isEmpty()) {
+            toastFun(this, "아이디/비밀번호를 확인해주세요.")
+        } else {
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.apply {
+                putExtra("extra_id", idET.text.toString())
+                putExtra("random", getRandomIndex())
+            }
+            toastFun(this, "로그인 성공")
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/week4_homework/SignUpActivity.kt
+++ app/src/main/java/com/example/week4_homework/SignUpActivity.kt
@@ -8,27 +8,33 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var nameET: EditText
+    private lateinit var idET: EditText
+    private lateinit var passwordET: EditText
+    private lateinit var signUpBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        val nameET = findViewById<EditText>(R.id.et_name_sign_up)
-        val idET = findViewById<EditText>(R.id.et_id_sign_up)
-        val passwordET = findViewById<EditText>(R.id.et_password_sign_up)
-        val signUpBtn = findViewById<Button>(R.id.btn_sign_up)
+        nameET = findViewById(R.id.et_name_sign_up)
+        idET = findViewById(R.id.et_id_sign_up)
+        passwordET = findViewById(R.id.et_password_sign_up)
+        signUpBtn = findViewById(R.id.btn_sign_up)
 
-        signUpBtn.setOnClickListener {
-            if (nameET.text.isEmpty() || idET.text.isEmpty() || passwordET.text.isEmpty()) {
-                toastFun(applicationContext, "입력되지 않은 정보가 있습니다.")
-            } else {
-                //데이터 저장 추가한다면 여기에 작성
-                val intent = Intent().apply {
-                    putExtra("signed_id", idET.text.toString())
-                    putExtra("signed_password", passwordET.text.toString())
-                }
-                setResult(Activity.RESULT_OK, intent)
-                finish()
+        signUpBtn.setOnClickListener { signUpClickListener() }
+    }
+
+    private fun signUpClickListener() {
+        if (nameET.text.isEmpty() || idET.text.isEmpty() || passwordET.text.isEmpty()) {
+            toastFun(applicationContext, "입력되지 않은 정보가 있습니다.")
+        } else {
+            //데이터 저장 추가한다면 여기에 작성
+            val intent = Intent().apply {
+                putExtra("signed_id", idET.text.toString())
+                putExtra("signed_password", passwordET.text.toString())
             }
+            setResult(Activity.RESULT_OK, intent)
+            finish()
         }
     }
 }
