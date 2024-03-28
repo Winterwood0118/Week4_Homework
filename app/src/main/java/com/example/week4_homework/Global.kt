e5cde7556ca07e77e7d8f0374d4ce828b61dee5b
WinterWood0118 <dltjdals125@gmail.com>
2024-03-28 15:33:35 +0900

    3.5.2
    정렬

--- app/src/main/java/com/example/week4_homework/Global.kt
+++ app/src/main/java/com/example/week4_homework/Global.kt
@@ -4,16 +4,13 @@ import android.content.Context
 import android.widget.Toast
 
 val imageResources = arrayOf(
-    R.drawable.img_1,
-    R.drawable.img_2,
-    R.drawable.img_3,
-    R.drawable.img_4,
-    R.drawable.img_5
+    R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4, R.drawable.img_5
 )
-fun getRandomIndex(): Int{
+
+fun getRandomIndex(): Int {
     return (imageResources.indices).random()
 }
 
-fun toastFun(context: Context, toast: String){
-    Toast.makeText(context,toast, Toast.LENGTH_SHORT).show()
+fun toastFun(context: Context, toast: String) {
+    Toast.makeText(context, toast, Toast.LENGTH_SHORT).show()
 }
